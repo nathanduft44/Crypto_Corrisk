256c27e7f1bf3b677ef81c7a9720bd1133194ba5
Ray Hurst <rhurst1125@berkeley.edu>
2021-08-20 15:33:29 -0700

    still dealing with commit issue this is a test

---
+++ App_Deployment_Round_1/CLI/cli_rd1.py
@@ -0,0 +1,41 @@
+import csv
+from pathlib import Path
+import questionary
+import fire
+
+
+def load_csv(csvpath):
+    with open(csvpath, "r") as csvfile:
+        data = []
+        csvreader = csv.reader(csvfile, delimiter=",")
+
+        # Skip the CSV Header
+        
+
+        # Read the CSV data
+        for row in csvreader:
+            data.append(row)
+    return data
+
+
+def load_crypto_data(file_path):
+    csvpath = Path(file_path)
+    return load_csv(csvpath)
+
+# def type_of_token():
+#     choice = questionary.select("choose:", choices=["BTC", "ETH" , "LTC", "ADA", "XLM" , "XRP", "ZEC" , "USDT" , "DASH"]).ask()
+#     print(f"You selected {choice}")
+
+def year_choice():
+    choice = questionary.select("choose:", choices=["2017", "2018" , "2019", "2020"]).ask()
+    print(f"You selected {choice}")
+
+
+def run():
+    file_path = Path("../2019_AM_rd1/2019_data.csv")
+    crypto_data = load_crypto_data(file_path)
+    year_choice()
+    market_variance = market_var_value
+
+if __name__ == "__main__":
+    fire.Fire(run)
